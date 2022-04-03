7c1e52e66d24614e0cb51a7005a6c16bb58ab445
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2022-04-03 14:26:18 -0500

    Create BD

---
+++ LeratoShop/LeratoShop/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using LeratoShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeratoShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+             
+        public DbSet<Platform> Platforms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Platform>().HasIndex(p => p.Name).IsUnique();
+        }
+
+    }
+
+}
---
+++ LeratoShop/LeratoShop/Migrations/20220403183702_AddIndexToPlatform.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeratoShop.Migrations
+{
+    public partial class AddIndexToPlatform : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Platforms_Name",
+                table: "Platforms",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Platforms_Name",
+                table: "Platforms");
+        }
+    }
+}
