c0abf6622e766678d3ccf7400015a787ff56b99a
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2022-05-31 22:13:29 -0500

    add fields to sales record

--- LeratoShop/LeratoShop/Models/SalesViewModel.cs
+++ LeratoShop/LeratoShop/Models/SalesViewModel.cs
@@ -23,6 +23,34 @@ namespace LeratoShop.Models
         [Range(1, int.MaxValue, ErrorMessage = "Debes de seleccionar un detalle del producto.")]
         public int ProductDetailsId { get; set; }
 
+        public string Id { get; set; }
+
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; }
+
+        [Display(Name = "Nombre Cliente")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Address { get; set; }
+
+        [Display(Name = "Teléfono")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string PhoneNumber { get; set; }
+
+
         public IEnumerable<SelectListItem> ProductDetails { get; set; }
     }
 }
